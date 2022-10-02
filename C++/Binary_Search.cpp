7b8b476d0ceff9995f57a17450cb5564f0b8af9d
Rohan Kumar <104218030+rohan12k@users.noreply.github.com>
2022-10-03 01:01:47 +0530

    Updated binary search soln

--- C++/Binary_Search.cpp
+++ C++/Binary_Search.cpp
@@ -6,11 +6,21 @@ class Solution {
   public:
     int binarysearch(int arr[], int n, int k) {
         // code here
-        for(int i=0;i<n;i++){
-            if(arr[i] == k){
-                return i;
-            }
-        }
-        return -1;
+      //this is linear search O(n)
+//         for(int i=0;i<n;i++){
+//             if(arr[i] == k){
+//                 return i;
+//             }
+//         }
+//         return -1;
+      //this is binary search O(logn)
+      int low=0,high=n-1;
+      while(low<high)
+      { int mid=(low+high)/2;
+        if(arr[mid]==k)return mid;
+       if(arr[mid]>k)low=mid+1;
+       else high=mid-1;
+      }
+      return -1; //if element not found
     }
 };
